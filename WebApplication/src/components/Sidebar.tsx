1f3aacbf6c558bd88aaf96cc052e07eeafaa2a68
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2025-02-10 23:16:39 +0700

    webapp: further extract Logo from Sidebar

--- WebApplication/src/components/Sidebar.tsx
+++ WebApplication/src/components/Sidebar.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
 import { SidebarItem } from "@/lib/types";
 
+function Logo() {
+	return <h1 className="text-2xl font-bold mb-6">Containerised POS Management</h1>;
+}
+
 export default function ({ items, activeTab, setActiveTab }: { items: SidebarItem[], activeTab: string, setActiveTab: (tab: string) => void }) {
 	return (
 		<div className="w-64 bg-white shadow-md p-4">
-			<h1 className="text-2xl font-bold mb-6">Management Portal</h1>
+			<Logo />
 			<nav className="space-y-2">
 				{items.map(({ icon: Icon, label }) => (
 					<Button
